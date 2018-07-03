fb27354b0fa1ffd32b2c7d71afd272bf4fb0d662
seavty <seavty.oeung@gmail.com>
2018-07-03 17:41:55 +0700

    doing uload excel

--- ChipMongWebApp/ChipMongWebApp/Controllers/SaleOrderController.cs
+++ ChipMongWebApp/ChipMongWebApp/Controllers/SaleOrderController.cs
@@ -2,6 +2,7 @@
 using ChipMongWebApp.Helpers;
 using ChipMongWebApp.Models.DTO.SaleOrder;
 using ChipMongWebApp.Utils.Attribute;
+using ClosedXML.Excel;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -94,6 +95,64 @@ namespace ChipMongWebApp.Controllers
                 Response.StatusCode = 400;
                 return ex.Message;
             }
+        }
+
+        //-> Edit
+        [HttpPost]
+        //[ValidateAntiForgeryToken]
+        public async Task<JsonResult> UploadExcel(SaleOrderUploadExcel uploadExcel)
+        {
+            if (ModelState.IsValid)
+            {
+
+                if (uploadExcel.ExcelFile.ContentLength > 0)
+                {
+                    if (uploadExcel.ExcelFile.FileName.EndsWith(".xlsx") || uploadExcel.ExcelFile.FileName.EndsWith(".xls"))
+                    {
+                        XLWorkbook Workbook;
+                        try//incase if the file is corrupt
+                            {
+                            Workbook = new XLWorkbook(uploadExcel.ExcelFile.InputStream);
+                        }
+                            catch (Exception ex)
+                        {
+                            ModelState.AddModelError(String.Empty, $"Check your file. {ex.Message}");
+                            return null;
+                        }
+                        IXLWorksheet WorkSheet = null;
+                        try//incase if the sheet you are looking for is not found
+                            {
+                            WorkSheet = Workbook.Worksheet("sheet1");
+
+                        }
+                            catch
+                        {
+                            ModelState.AddModelError(String.Empty, "sheet not found!");
+                            return null;
+                        }
+                        WorkSheet.FirstRow().Delete();//if you want to remove ist row
+
+                        foreach (var row in WorkSheet.RowsUsed())
+                        {
+                            //do something here
+                            var soNumber = row.Cell(1).Value.ToString();//Get ist cell. 1 represent column number
+                            var soStatus = row.Cell(2).Value.ToString();
+
+                        }
+                    }
+                    else
+                    {
+                        ModelState.AddModelError(String.Empty, "Only .xlsx and .xls files are allowed");
+                        return null;
+                    }
+                }
+                else
+                {
+                    ModelState.AddModelError(String.Empty, "Not a valid file");
+                    return null;
+                }
+            }
+            return null;
         }
 
     }
